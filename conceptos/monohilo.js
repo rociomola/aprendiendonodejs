a33151e7d969a93a19121bb99442fff8d110d507
rociomola <Rociomola@yahoo.es>
2021-03-09 10:26:54 +0100

    Create monohilo.js
    aprendiendo como funciona el monohilo de nodejs

---
+++ conceptos/monohilo.js
@@ -0,0 +1,10 @@
+console.log('Hola,mundo');
+let i=0;
+setInterval(function() {
+    console.log(i);
+    i++;
+    if (i === 5){
+        console.log('forzando error');
+        var a = 3+z;
+    }
+},1000);
