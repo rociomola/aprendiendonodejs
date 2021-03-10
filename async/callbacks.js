b6fc719c8c3744d71bdca4b466f44ff7883479cb
rociomola <Rociomola@yahoo.es>
2021-03-10 09:21:33 +0100

    Update callbacks.js
    final leccion 7

--- async/callbacks.js
+++ async/callbacks.js
@@ -2,18 +2,18 @@ function saludo(nombre, micallback ){
     
     setTimeout(function()  {
         console.log('Hola,'+nombre);
-        micallback();
+        micallback(nombre);
     }, 1000);
 }
 function despedida (nombre, otrocallback){
     setTimeout(function() {
-        console.log('Adios, ',nombre);
+        console.log('Adios,',nombre);
         otrocallback();
     }, 1000);
 }
 console.log('Iniciando proceso');
-saludo('Rocio',function(){
-    despedida('Rocio' ,function(){
+saludo('Rocio',function(nombre){
+    despedida(nombre ,function(){
        console.log('Terminamdo proceso');
     });
 });
